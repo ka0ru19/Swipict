fb2dbc4647a3efb41a0c8df6d15880385e5af5a9
ka0ru19 <k.prfm19@gmail.com>
2018-11-17 15:14:50 +0900

    Initial Commit

---
+++ Swipict/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swipict
+//
+//  Created by Wataru Inoue on 2018/11/17.
+//  Copyright © 2018 Wataru Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
